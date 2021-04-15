04db153ff2fb778bbd057b2c90188053ee56ec4b
Masterchef365 <duncan.freeman1@gmail.com>
2021-04-15 16:52:58 -0700

    List

--- calc.py
+++ calc.py
@@ -1,11 +1,14 @@
 def calc(a, b):
-    sum = a + b
+    sum_ = a + b
     diff = a - b
     mul = a * b
     div = a / b
-    print(sum)
+    print(sum_)
     print(diff)
     print(mul)
     print(div)
+    ls = [sum_, diff, mul, div]
+    print(ls)
+
 
 calc(4, 3)
